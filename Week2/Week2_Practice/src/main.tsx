b9762d9926e6cde20f1e87a173cbf7182bd922b4
Jungbin906 <jungbin906@naver.com>
2026-03-26 19:23:32 +0900

    [week2/fix] Lint, Biome 에러 수정 2차

--- Week2/Week2_Practice/src/main.tsx
+++ Week2/Week2_Practice/src/main.tsx
@@ -2,8 +2,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { CounterProvider } from "./context/CounterProvider.tsx";
+import App from "./App";
+import { CounterProvider } from "./context/CounterProvider";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
