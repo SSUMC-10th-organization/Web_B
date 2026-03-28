eb6857ae2a5851ca37869c5598b2c813cfe146ce
yseungji <ysj9074_@naver.com>
2026-03-28 21:34:13 +0900

    [week3/misson] useEffect를 활용한 영화 목록 페이지 구현과 React Router와 페이지네이션을 활용한 동적 라우팅

---
+++ tony/Week3/Mission2/src/App.tsx
@@ -0,0 +1,33 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+//createBrowerRouter v6
+import HomePage from './pages/home';
+import NotFound from './pages/not-found';
+import Movies from './pages/movies';
+import RootLayout from './layout/root-layout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        // /movies/뒤에 오는 값을 movieId라는 이름으로 받겠다는 뜻
+        path: 'movies/:category',
+        element: <Movies />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
