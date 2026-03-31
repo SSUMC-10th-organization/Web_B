a0d2de29fb397fd429a0ff2705001707df952db9
Jihyeon Lee <loveljh0109@naver.com>
2026-03-31 21:45:51 +0900

    [week3/mission] useParams를 활용한 영화 상세 페이지 개발

--- Week3/movie-list/src/pages/MovieDetailPage.tsx
+++ Week3/movie-list/src/pages/MovieDetailPage.tsx
@@ -1,9 +1,69 @@
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { Movie } from "../types/movie";
+import axios from "axios";
+import { LoadingSpinner } from "../components/LoadingSpinner";
 
 const MovieDetailPage = () => {
-  const params = useParams();
-  console.log(params);
-  return <div>MovieDetailPage</div>;
+  const { movieId } = useParams<{ movieId: string }>();
+
+  const [movie, setMovie] = useState<Movie | undefined>(undefined);
+  const [isPending, setIsPending] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchMovieDetail = async () => {
+      setIsPending(true);
+      try {
+        const { data } = await axios.get<Movie>(
+          `https://api.themoviedb.org/3/movie/${movieId}?language=ko-KR`,
+          {
+            headers: {
+              Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+            },
+          },
+        );
+        setMovie(data);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsPending(false);
+      }
+    };
+
+    fetchMovieDetail();
+  }, [movieId]);
+
+  if (isError) {
+    return (
+      <div>
+        <span className="text-red-500 text-2xl">에러가 발생했습니다.</span>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        <img
+          src={`https://image.tmdb.org/t/p/w200${movie?.poster_path}`}
+          alt="movie poster"
+        />
+      </div>
+      <div>
+        <h1>{movie?.title}</h1>
+        <p>{movie?.vote_average.toFixed(1)}</p>
+        <p>{movie?.release_date}</p>
+
+        <p>{movie?.overview}</p>
+      </div>
+      {isPending && (
+        <div className="flex items-center justify-center h-dvh">
+          <LoadingSpinner />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default MovieDetailPage;
