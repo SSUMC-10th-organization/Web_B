8abf2e09f8434375e1d6d2ea102dffc22ba709d4
Jihyeon Lee <loveljh0109@naver.com>
2026-03-31 20:46:19 +0900

    [week3/mission] React Router와 페이지네이션을 통한 동적 영화 목록 구현

--- Week3/movie-list/src/App.tsx
+++ Week3/movie-list/src/App.tsx
@@ -1,12 +1,30 @@
 import "./App.css";
+import HomePage from "./pages/Homepage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
 import MoviePage from "./pages/MoviePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "movies/:category",
+        element: <MoviePage />,
+      },
+      {
+        path: "movies/:category/:movieId",
+        element: <MovieDetailPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <MoviePage />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ Week3/movie-list/src/pages/MovieDetailPage.tsx
@@ -0,0 +1,5 @@
+const MovieDetailPage = () => {
+  return <div></div>;
+};
+
+export default MovieDetailPage;
