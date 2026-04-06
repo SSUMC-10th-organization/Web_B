5b2f80b77e15c23c1fba50edceb8acf17a5aba76
Jihyeon Lee <loveljh0109@naver.com>
2026-04-06 14:04:16 +0900

    [week3/refactor] MovieDetailPage 출연진 구현 및 피드백 반영

--- Week3/movie-list/src/App.tsx
+++ Week3/movie-list/src/App.tsx
@@ -1,30 +1,30 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import MovieDetailPage from "./pages/MovieDetailPage";
 import MoviePage from "./pages/MoviePage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <HomePage />,
-		errorElement: <NotFoundPage />,
-		children: [
-			{
-				path: "movies/:category",
-				element: <MoviePage />,
-			},
-			{
-				path: "movie/:movieId",
-				element: <MovieDetailPage />,
-			},
-		],
-	},
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: "movies/:category",
+        element: <MoviePage />,
+      },
+      {
+        path: "movie/:movieId",
+        element: <MovieDetailPage />,
+      },
+    ],
+  },
 ]);
 
 function App() {
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
