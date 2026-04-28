bef37a19db35b9c80de2539ddd06d9c2aa460f00
Jihyeon Lee <loveljh0109@naver.com>
2026-04-28 10:49:32 +0900

    [Week5] Refresh Token을 활용하여 지속적인 로그인 유지하기

--- stella/Week5/Chapter-5/src/apis/axios.ts
+++ stella/Week5/Chapter-5/src/apis/axios.ts
@@ -1,19 +1,108 @@
-import type { AxiosInstance } from "axios";
+import type { AxiosInstance, InternalAxiosRequestConfig } from "axios";
 import axios from "axios";
 import { LOCAL_STORAGE_KEY } from "../constants/key";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+
+interface CustomInternalAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean;
+}
+
+let refreshPromise: Promise<string> | null = null;
 
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_API_URL,
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const { getItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
-  const token = getItem();
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const { getItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+    const accesstoken = getItem();
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (accesstoken) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${accesstoken}`;
+    }
 
-  return config;
-});
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest: CustomInternalAxiosRequestConfig = error.config;
+
+    if (
+      error.reponse &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      if (originalRequest.url === "/v1/auth/refresh") {
+        const { removeItem: removeAccessToken } = useLocalStorage(
+          LOCAL_STORAGE_KEY.accessToken,
+        );
+        const { removeItem: removeRefreshToken } = useLocalStorage(
+          LOCAL_STORAGE_KEY.refreshToken,
+        );
+        removeAccessToken();
+        removeRefreshToken();
+        window.location.href = "/login";
+        return Promise.reject(error);
+      }
+
+      // 재시도 플래그 설정
+      originalRequest._retry = true;
+
+      // 이미 리프레시 요청이 진행 중이면 그 Promise를 재사용
+      if (!refreshPromise) {
+        // refresh 요청 실행 후, Promise를 전역 변수에 할당
+        refreshPromise = (async () => {
+          const { getItem: getRefreshToken } = useLocalStorage(
+            LOCAL_STORAGE_KEY.refreshToken,
+          );
+          const refreshToken = getRefreshToken();
+          const { data } = await axiosInstance.post("/v1/auth/refresh", {
+            refresh: refreshToken,
+          });
+
+          // 새 토큰 반환
+          const { setItem: setAccessToken } = useLocalStorage(
+            LOCAL_STORAGE_KEY.accessToken,
+          );
+          const { setItem: setRefreshToken } = useLocalStorage(
+            LOCAL_STORAGE_KEY.refreshToken,
+          );
+          setAccessToken(data.data.accessToken);
+          setRefreshToken(data.data.refreshToken);
+
+          // 새 accessToken을 반환하여 다른 요청들이 이것을 사용할 수 있게 함.
+          return data.data.accessToken;
+        })()
+          .catch((error) => {
+            const { removeItem: removeAccessToken } = useLocalStorage(
+              LOCAL_STORAGE_KEY.accessToken,
+            );
+
+            const { removeItem: removeRefreshToken } = useLocalStorage(
+              LOCAL_STORAGE_KEY.refreshToken,
+            );
+            removeAccessToken();
+            removeRefreshToken();
+          })
+          .finally(() => {
+            refreshPromise = null;
+          });
+      }
+
+      // 진행중인 refreshPromise가 해결될 때까지 기다림
+      return refreshPromise.then((newAccessToken) => {
+        // 원본 요청의 Authorization 헤더를 갱신된 토큰으로 업뎃
+        originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+        // 업데이트 된 원본 요청 재시도
+        return axiosInstance.request(originalRequest);
+      });
+    }
+    return Promise.reject(error);
+  },
+);
