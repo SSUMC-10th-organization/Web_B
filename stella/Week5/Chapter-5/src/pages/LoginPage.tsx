8852eea953de180123b2d2bdde7edbe76fee104b
Jihyeon Lee <loveljh0109@naver.com>
2026-04-28 22:28:31 +0900

    [Week5/mission] Social Login Google 구현

--- stella/Week5/Chapter-5/src/pages/LoginPage.tsx
+++ stella/Week5/Chapter-5/src/pages/LoginPage.tsx
@@ -27,6 +27,11 @@ export const LoginPage = () => {
     navigate("/my");
   };
 
+  const handleGoogleLogin = () => {
+    window.location.href =
+      import.meta.env.VITE_SERVER_API_URL + "/v1/auth/google/login";
+  };
+
   const isDisabled =
     Object.values(errors || {}).some((error) => error.length > 0) ||
     Object.values(values).some((value) => value === "");
@@ -59,6 +64,20 @@ export const LoginPage = () => {
         className="w-[300px] p-[10px] bg-black text-white py-3 rounded-md text-lg font-medium hover:bg-black transition-colors cursor-pointer disabled:bg-gray-300"
       >
         로그인
+      </button>
+      <button
+        type="button"
+        onClick={handleGoogleLogin}
+        className="w-[300px] p-[10px] bg-black text-white py-3 rounded-md text-lg font-medium hover:bg-black transition-colors cursor-pointer disabled:bg-gray-300"
+      >
+        <div className="flex items-center justify-center gap-4">
+          <img
+            src={"/images/google-color.svg"}
+            alt="Google Logo Image"
+            className="w-8 h-8"
+          />
+          <span>구글 로그인</span>
+        </div>
       </button>
     </div>
   );
