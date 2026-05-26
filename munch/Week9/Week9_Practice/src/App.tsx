24cf91f2304c7f227204ecbbe08d982ef9915504
ByungMMin <149650808+ByungMMin@users.noreply.github.com>
2026-05-26 17:25:36 +0900

    [week9/practice]fix code

--- munch/Week9/Week9_Practice/src/App.tsx
+++ munch/Week9/Week9_Practice/src/App.tsx
@@ -2,7 +2,11 @@ import "./App.css";
 import UseReducerCompany from "./pages/UseReducerCompany";
 
 function App() {
-	return <UseReducerCompany />;
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen">
+			<UseReducerCompany />
+		</div>
+	);
 }
 
 export default App;
